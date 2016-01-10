1652dd87398131c29f60649e1184326c7fc7952d
emyarod <allllaboutyou@gmail.com>
2016-01-09 20:03:17 -0800

    add example key module

---
+++ keys.example.js
@@ -0,0 +1,8 @@
+var keys = {
+  clientKey: '', // OAuth client key (provided at reddit app)
+  secret: '', // OAuth secret (provided at reddit app)
+  username: '', // Reddit username used to make the reddit app
+  password: '', // Reddit password for the username
+};
+
+module.exports = keys;
