1f11fb865d8a42a4f0f17637e0023f20ba97efde
emyarod <allllaboutyou@gmail.com>
2016-01-10 02:57:56 -0800

    grab top 5 nominations and scores from each award category post

--- script.js
+++ script.js
@@ -14,38 +14,40 @@ var reddit = new Snoocore({
 });
 
 var awardTitle;
+var postIDs = [];
 
 // create output file
-fs.writeFileSync('out.md', '');
+fs.writeFileSync('results.md', '');
 
-// reddit('/api/v1/me').get().then(function(result) {
-//   console.log(result);
-// });
+reddit('/r/annualkpopawards/new').listing({ limit: 100 }).then(function(slice) {
+  // get all award category post IDs
+  for (var i = 0; i < slice.children.length; i++) {
+    postIDs.push(slice.children[i].data.id);
+  }
 
-// reddit('/r/annualkpopawards/new').listing({ limit: 100 }).then(function(slice) {
-//   for (var i = 0; i < slice.children.length; i++) {
-//     // console.log(slice.children[i].data.url);
-//   }
-//   // console.log(slice.children);
-// });
+  // get top 5 nominations from each award category
+  for (var i = 0; i < postIDs.length; i++) {
+    reddit('/r/annualkpopawards/comments/' + postIDs[i]).get().then(function(result) {
+      // result[0] is the OP, result[1] is the comments section
+      awardTitle = '__' + result[0].data.children[0].data.title + '__';
 
-reddit('/r/annualkpopawards/comments/3uc10o').get().then(function(result) {
-  // result[0] is the OP, result[1] is the comments section
-  awardTitle = '__' + result[0].data.children[0].data.title + '__';
+      // write award title to file
+      fs.appendFileSync('results.md', '\n' + awardTitle + '\n\n');
 
-  // write award title to file
-  fs.appendFileSync('out.md', awardTitle + '\n\n');
-
-  // write nominations and scores to file
-  for (var i = 0; i < result[1].data.children.length; i++) {
-    var nomination = result[1].data.children[i].data.body;
-    var upvotes = result[1].data.children[i].data.ups;
-    if(i == 0) {
-      fs.appendFileSync('out.md', '[](//#award2)' + nomination + ' | ' + upvotes + '\n' + ':|:' + '\n');
-    } else if (i == 1) {
-      fs.appendFileSync('out.md', '[](//#trophy)' + nomination + ' | ' + upvotes + '\n');
-    } else {
-      fs.appendFileSync('out.md', nomination + ' | ' + upvotes + '\n');
-    }
+      // write top 5 nominations and scores to file
+      for (var i = 0; i < 5; i++) {
+        var nomination = result[1].data.children[i].data.body;
+        var upvotes = result[1].data.children[i].data.ups;
+        if (i == 0) {
+          // give top nomination winner's trophy
+          fs.appendFileSync('results.md', '[](//#award2)' + nomination + ' | ' + upvotes + '\n' + ':|:' + '\n');
+        } else if (i == 1) {
+          // give first runner up smaller trophy
+          fs.appendFileSync('results.md', '[](//#trophy)' + nomination + ' | ' + upvotes + '\n');
+        } else {
+          fs.appendFileSync('results.md', nomination + ' | ' + upvotes + '\n');
+        }
+      }
+    });
   }
 });
